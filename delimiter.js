2566eb1f38bd773bad3c4c834486f2f9c293e11c
Chadwick Butts <ccbutts@mail.usf.edu>
2019-11-11 19:07:50 -0500

    improved how the text is delimited and added a button to clear text fields

--- delimiter.js
+++ delimiter.js
@@ -2,17 +2,34 @@
     let leftTextArea = document.querySelector("#leftTextArea");
     let rightTextArea = document.querySelector("#rightTextArea");;
     let delimitBtn = document.querySelector("#delimitBtn");
-    let rightToLeftBtn;
+    let clearBtn = document.querySelector("#clearBtn");
+    let removeNewLineChkbx = document.querySelector("#removeNewLineChkbx");
+
+
 
     function delimitText() {
-        let userText = leftTextArea.value.trim();
-        console.log(userText);
+        let userText = leftTextArea.value;
         let delimiter = document.querySelector("#delimitingChar").value;
+        userText = userText.split(new RegExp(/\n/, 'g'));
 
-        userText = userText.split(new RegExp(/\s/, 'g'));
-        console.log(userText);
-        rightTextArea.value = userText.join(delimiter.toString());
+        console.log(removeNewLineChkbx.checked);
+        if (removeNewLineChkbx.checked) {
+            rightTextArea.value = userText.join(delimiter.toString());
+        } else {
+            rightTextArea.value = userText.join(delimiter.toString() + '\n');
+        }
+
+
+    }
+
+    
+    function clearTextAreas() {
+        leftTextArea.value = "";
+        leftTextArea.innerHTML = "";
+        rightTextArea.value = "";
+        rightTextArea.innerHTML = "";
     }
 
     delimitBtn.addEventListener("click", delimitText);
+    clearBtn.addEventListener("click", clearTextAreas);
 })();
