25b5efddb0264ba96c7d86825917c66df2d0b876
Chad Butts <chadbutts@MacBook-Pro-2.local>
2019-11-18 01:05:30 -0500

    added explode button and spaces/new line options to drop-down

--- delimiter.js
+++ delimiter.js
@@ -2,27 +2,50 @@
     let leftTextArea = document.querySelector("#leftTextArea");
     let rightTextArea = document.querySelector("#rightTextArea");;
     let delimitBtn = document.querySelector("#delimitBtn");
+    let explodeBtn = document.querySelector("#explodeBtn");
     let clearBtn = document.querySelector("#clearBtn");
     let removeNewLineChkbx = document.querySelector("#removeNewLineChkbx");
-
-
-
+    let removeDuplicatesChkbx = document.querySelector("#removeDuplicatesChkbx");
+    let settingsBtn = document.querySelector("#settingsBtn");
+    let settings = document.querySelector("#settingsSection");
+    
+    
     function delimitText() {
         let userText = leftTextArea.value;
         let delimiter = document.querySelector("#delimitingChar").value;
         userText = userText.split(new RegExp(/\n/, 'g'));
 
-        console.log(removeNewLineChkbx.checked);
+        if (removeDuplicatesChkbx) {
+            userText = Array.from(new Set(userText));
+        } 
+
         if (removeNewLineChkbx.checked) {
-            rightTextArea.value = userText.join(delimiter.toString());
+            userText = userText.join(delimiter.toString());
         } else {
-            rightTextArea.value = userText.join(delimiter.toString() + '\n');
+            userText = userText.join(delimiter.toString() + '\n');
         }
 
+        rightTextArea.value = userText;
 
     }
-
     
+    function explodeText() {
+        
+        let userText = leftTextArea.value;
+        let delimiter = document.querySelector("#delimitingChar").value;
+        userText = userText.split(new RegExp(/\W/, 'g'));
+
+        console.log(userText);
+        if (delimiter === "new line") {
+            userText = userText.join('\n');
+        } else if (delimiter === " ") {
+
+            userText = userText.join(" ");
+        }
+
+        rightTextArea.value = userText;
+    }
+
     function clearTextAreas() {
         leftTextArea.value = "";
         leftTextArea.innerHTML = "";
@@ -30,6 +53,11 @@
         rightTextArea.innerHTML = "";
     }
 
+
     delimitBtn.addEventListener("click", delimitText);
+    explodeBtn.addEventListener("click", explodeText);
     clearBtn.addEventListener("click", clearTextAreas);
+    settingsBtn.addEventListener("click", function() {
+        settings.classList.toggle("hide");
+    })
 })();
